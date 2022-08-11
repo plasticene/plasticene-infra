c3278ec801c0a4608e33f6941738020a7f1be9d4
fjzheng <1059959730@qq.com>
2022-08-11 18:35:52 +0800

    add: :sparkles: 扫描dao包

--- short-url-service/src/main/java/com/plasticene/shorturl/ShortUrlServiceApplication.java
+++ short-url-service/src/main/java/com/plasticene/shorturl/ShortUrlServiceApplication.java
@@ -1,9 +1,11 @@
 package com.plasticene.shorturl;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.plasticene.shorturl.dao")
 public class ShortUrlServiceApplication {
 
     public static void main(String[] args) {
