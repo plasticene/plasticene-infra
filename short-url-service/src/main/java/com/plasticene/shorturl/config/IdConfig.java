6e36c7009c0382a6f9e2a7402b9085e683b9a656
fjzheng <1059959730@qq.com>
2022-08-12 16:18:30 +0800

    add: :sparkles: 注入分布式生成器

---
+++ short-url-service/src/main/java/com/plasticene/shorturl/config/IdConfig.java
@@ -0,0 +1,28 @@
+package com.plasticene.shorturl.config;
+
+import cn.hutool.core.util.IdUtil;
+import com.plasticene.boot.common.utils.IdGenerator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/8/12 15:36
+ */
+@Configuration
+public class IdConfig {
+
+    @Value("${ptc.datacenter-id : 1}")
+    private Long datacenterId;
+    @Value("${ptc.worker-id : 1}")
+    private Long workerId;
+
+    @Bean
+    public IdGenerator idGenerator() {
+        IdGenerator idGenerator = new IdGenerator(datacenterId, workerId);
+        return idGenerator;
+    }
+
+}
