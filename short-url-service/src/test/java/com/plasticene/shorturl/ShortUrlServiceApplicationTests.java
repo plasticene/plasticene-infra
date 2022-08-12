a595821de1d2eb8a2a71b1d2da20f954b90568d7
fjzheng <1059959730@qq.com>
2022-08-12 16:23:25 +0800

    add: :white_check_mark: 测试用例

--- short-url-service/src/test/java/com/plasticene/shorturl/ShortUrlServiceApplicationTests.java
+++ short-url-service/src/test/java/com/plasticene/shorturl/ShortUrlServiceApplicationTests.java
@@ -1,13 +1,33 @@
 package com.plasticene.shorturl;
 
-import org.junit.jupiter.api.Test;
+import com.plasticene.shorturl.service.ShortUrlService;
+import com.plasticene.shorturl.service.UniqueCodeService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
-class ShortUrlServiceApplicationTests {
+@RunWith(SpringRunner.class)
+public class ShortUrlServiceApplicationTests {
+
+    @Resource
+    private ShortUrlService shortUrlService;
+    @Resource
+    private UniqueCodeService uniqueCodeService;
+
+
+    void contextLoads() {
+    }
+
+
+
 
     @Test
-    void contextLoads() {
+    public void generateCode() {
+        uniqueCodeService.generateUniqueCode();
     }
 
 }
