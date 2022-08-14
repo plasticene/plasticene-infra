1adbf8660b4b4345a25aa2483cade10dc78aedbb
fjzheng <1059959730@qq.com>
2022-08-14 23:45:04 +0800

    update: :sparkles: 添加异步生成压缩码逻辑

--- short-url-service/src/main/java/com/plasticene/shorturl/service/impl/UniqueCodeServiceImpl.java
+++ short-url-service/src/main/java/com/plasticene/shorturl/service/impl/UniqueCodeServiceImpl.java
@@ -6,6 +6,7 @@ import com.plasticene.shorturl.dao.UniqueCodeDAO;
 import com.plasticene.shorturl.entity.UniqueCode;
 import com.plasticene.shorturl.service.UniqueCodeService;
 import com.plasticene.shorturl.utils.RandomUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
 
 /**
  * @author fjzheng
@@ -28,16 +30,38 @@ public class UniqueCodeServiceImpl extends ServiceImpl<UniqueCodeDAO, UniqueCode
     private StringRedisTemplate stringRedisTemplate;
     @Resource
     private IdGenerator idGenerator;
+    @Resource
+    private ExecutorService executorService;
+
+    @Value("${unique-code.max-size}")
+    private Integer maxSize;
+    @Value("${unique-code.min-size}")
+    private Integer minSize;
 
 
     private static final String UNIQUE_CODE_KEY = "short_url_unique_code";
 
 
+
+    @Override
+    public String getUniqueCode() {
+        String code = stringRedisTemplate.opsForSet().pop(UNIQUE_CODE_KEY);
+        asyncGenerateUniqueCode();
+        return code;
+    }
+
+    @Override
+    public Set<String> getUniqueCode(Integer size) {
+        List<String> codes = stringRedisTemplate.opsForSet().pop(UNIQUE_CODE_KEY, size);
+        asyncGenerateUniqueCode();
+        return new HashSet<>(codes);
+    }
+
     @Override
     @Transactional(rollbackFor = Exception.class)
     public void generateUniqueCode() {
         Set<String> codes = new HashSet<>();
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < maxSize; i++) {
             String s = RandomUtils.generateCode(6);
             codes.add(s);
         }
@@ -52,15 +76,14 @@ public class UniqueCodeServiceImpl extends ServiceImpl<UniqueCodeDAO, UniqueCode
         saveBatch(uniqueCodeList);
     }
 
+    public void asyncGenerateUniqueCode() {
+        Long size = stringRedisTemplate.opsForSet().size(UNIQUE_CODE_KEY);
+        // 如果剩余压缩码数量小于最小数量，异步生成压缩码
+        if (size < minSize) {
+            executorService.execute(() -> {
+                generateUniqueCode();
+            });
+        }
 
-    @Override
-    public String getUniqueCode() {
-        String code = stringRedisTemplate.opsForSet().pop(UNIQUE_CODE_KEY);
-        return code;
-    }
-
-    @Override
-    public Set<String> getUniqueCode(Integer size) {
-        return null;
     }
 }
