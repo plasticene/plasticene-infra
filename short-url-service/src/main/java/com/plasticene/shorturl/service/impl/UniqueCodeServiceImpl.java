769ba6a68356e0cd0b86d2bc6f0bf0181988d75f
fjzheng <1059959730@qq.com>
2022-08-15 21:44:56 +0800

    add: :sparkles: 生成压缩码基于布隆过滤器判断是否存在

--- short-url-service/src/main/java/com/plasticene/shorturl/service/impl/UniqueCodeServiceImpl.java
+++ short-url-service/src/main/java/com/plasticene/shorturl/service/impl/UniqueCodeServiceImpl.java
@@ -1,16 +1,23 @@
 package com.plasticene.shorturl.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.plasticene.boot.common.pojo.PageParam;
+import com.plasticene.boot.common.pojo.PageResult;
 import com.plasticene.boot.common.utils.IdGenerator;
+import com.plasticene.shorturl.constant.CommonConstant;
 import com.plasticene.shorturl.dao.UniqueCodeDAO;
 import com.plasticene.shorturl.entity.UniqueCode;
+import com.plasticene.shorturl.filter.ShortUrlBloomFilter;
 import com.plasticene.shorturl.service.UniqueCodeService;
 import com.plasticene.shorturl.utils.RandomUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -25,13 +32,18 @@ import java.util.concurrent.ExecutorService;
  * @date 2022/8/12 14:26
  */
 @Service
+@Slf4j
 public class UniqueCodeServiceImpl extends ServiceImpl<UniqueCodeDAO, UniqueCode> implements UniqueCodeService {
+    @Resource
+    private UniqueCodeDAO uniqueCodeDAO;
     @Resource
     private StringRedisTemplate stringRedisTemplate;
     @Resource
     private IdGenerator idGenerator;
     @Resource
     private ExecutorService executorService;
+    @Resource
+    private ShortUrlBloomFilter shortUrlBloomFilter;
 
     @Value("${unique-code.max-size}")
     private Integer maxSize;
@@ -40,8 +52,6 @@ public class UniqueCodeServiceImpl extends ServiceImpl<UniqueCodeDAO, UniqueCode
 
 
     private static final String UNIQUE_CODE_KEY = "short_url_unique_code";
-
-
 
     @Override
     public String getUniqueCode() {
@@ -58,12 +68,32 @@ public class UniqueCodeServiceImpl extends ServiceImpl<UniqueCodeDAO, UniqueCode
     }
 
     @Override
+    public PageResult<UniqueCode> getUnusedList(PageParam pageParam) {
+        LambdaQueryWrapper<UniqueCode> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(UniqueCode::getStatus, CommonConstant.CODE_NOT_USE);
+        queryWrapper.select(UniqueCode::getCode);
+        PageResult<UniqueCode> pageResult = uniqueCodeDAO.selectPage(pageParam, queryWrapper);
+        return pageResult;
+    }
+
+    @Override
     @Transactional(rollbackFor = Exception.class)
     public void generateUniqueCode() {
+        log.info("==========开始生成压缩码===========");
+        long startTime = System.currentTimeMillis();
         Set<String> codes = new HashSet<>();
+        Set<String> existCodes = new HashSet<>();
         for (int i = 0; i < maxSize; i++) {
-            String s = RandomUtils.generateCode(6);
-            codes.add(s);
+            String code = RandomUtils.generateCode(6);
+            Boolean exist = shortUrlBloomFilter.isExist(code);
+            if (exist) {
+                existCodes.add(code);
+            } else {
+                codes.add(code);
+            }
+        }
+        if (!CollectionUtils.isEmpty(existCodes)) {
+            log.info("=========以下压缩码已存在：{}", existCodes);
         }
         stringRedisTemplate.opsForSet().add(UNIQUE_CODE_KEY, codes.toArray(new String[codes.size()]));
         List<UniqueCode> uniqueCodeList = new ArrayList<>();
@@ -74,6 +104,8 @@ public class UniqueCodeServiceImpl extends ServiceImpl<UniqueCodeDAO, UniqueCode
             uniqueCodeList.add(uniqueCode);
         });
         saveBatch(uniqueCodeList);
+        long costTime = System.currentTimeMillis() - startTime;
+        log.info("===============结束生成压缩码, costTime:[{}],Count:[{}]==============", costTime, codes.size());
     }
 
     public void asyncGenerateUniqueCode() {
