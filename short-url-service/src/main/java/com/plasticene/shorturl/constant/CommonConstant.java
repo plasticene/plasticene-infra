1e51311e3eaaa884c3a3a2dfe0708ec03aabe246
fjzheng <1059959730@qq.com>
2022-08-15 21:41:39 +0800

    add: :sparkles: 添加常量

---
+++ short-url-service/src/main/java/com/plasticene/shorturl/constant/CommonConstant.java
@@ -0,0 +1,13 @@
+package com.plasticene.shorturl.constant;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/8/15 10:23
+ */
+public interface CommonConstant {
+
+    Integer CODE_NOT_USE = 0;
+    Integer CODE_IS_USED = 1;
+    Integer CODE_IS_INVALID = -1;
+}
