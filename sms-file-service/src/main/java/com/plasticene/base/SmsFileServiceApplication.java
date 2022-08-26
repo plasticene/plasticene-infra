d26edb776bb3607792bea4fc478938a2df54cd7a
fjzheng <1059959730@qq.com>
2022-08-26 17:18:52 +0800

    add: :tada: 初始化短信&文件服务

---
+++ sms-file-service/src/main/java/com/plasticene/base/SmsFileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.plasticene.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsFileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsFileServiceApplication.class, args);
+    }
+
+}
---
+++ sms-file-service/src/test/java/com/plasticene/base/SmsFileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plasticene.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsFileServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
