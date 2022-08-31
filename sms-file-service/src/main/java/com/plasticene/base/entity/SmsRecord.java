bc28ef19449a72c8493cb706a18d008b008f311b
fjzheng <1059959730@qq.com>
2022-08-31 17:43:10 +0800

    add: :sparkles: 添加短信记录

---
+++ sms-file-service/src/main/java/com/plasticene/base/entity/SmsRecord.java
@@ -0,0 +1,63 @@
+package com.plasticene.base.entity;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
+import com.plasticene.boot.mybatis.core.metadata.BaseDO;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/8/31 16:10
+ */
+@Data
+public class SmsRecord extends BaseDO {
+
+    private Long id;
+
+    private Long signId;
+
+    private String signCode;
+
+    private Long templateId;
+
+    private String templateCode;
+
+    private Integer templateType;
+
+    private String content;
+
+    @TableField(typeHandler = JacksonTypeHandler.class)
+    private Map<String, Object> params;
+
+    private String apiTemplateCode;
+
+    private String mobile;
+
+    private Integer sendStatus;
+
+    private Date sendTime;
+
+    private String sendMsg;
+
+    private String apiSendCode;
+
+    private String apiSendMsg;
+
+    private String apiRequestId;
+
+    private String apiSerialNo;
+
+    private Integer receiveStatus;
+
+    private Date receiveTime;
+
+    private String apiReceiveCode;
+
+    private String apiReceiveMsg;
+
+
+}
