9e5fbbee2f3fb704f6705b253d92920d515e4ed0
fjzheng <1059959730@qq.com>
2022-09-01 15:51:14 +0800

    add: :art: 扫描dao

--- sms-file-service/src/main/java/com/plasticene/base/SmsFileServiceApplication.java
+++ sms-file-service/src/main/java/com/plasticene/base/SmsFileServiceApplication.java
@@ -1,9 +1,11 @@
 package com.plasticene.base;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.plasticene.base.dao")
 public class SmsFileServiceApplication {
 
     public static void main(String[] args) {
