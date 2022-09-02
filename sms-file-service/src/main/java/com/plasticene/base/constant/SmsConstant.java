91f0e028edf4330239aa4210dc05d67273e18eee
fjzheng <1059959730@qq.com>
2022-09-02 17:18:24 +0800

    add: :sparkles: 添加短信常量

---
+++ sms-file-service/src/main/java/com/plasticene/base/constant/SmsConstant.java
@@ -0,0 +1,13 @@
+package com.plasticene.base.constant;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/9/2 16:29
+ */
+public interface SmsConstant {
+
+    String DEFAULT_PLACE_HOLDER_REGEX = "\\{[a-z].*?\\}";
+
+    String DEFAULT_PLACE_HOLDER_KEY_REGEX = "[^(\\{)|(\\})]+";
+}
