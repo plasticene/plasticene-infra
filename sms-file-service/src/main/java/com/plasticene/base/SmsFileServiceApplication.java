2b8a269f83402fbce1db8ec353f0d03639a7e755
fjzheng <1059959730@qq.com>
2022-09-08 16:09:11 +0800

    update: :art: 开启消息队列监听消费功能

--- sms-file-service/src/main/java/com/plasticene/base/SmsFileServiceApplication.java
+++ sms-file-service/src/main/java/com/plasticene/base/SmsFileServiceApplication.java
@@ -1,11 +1,13 @@
 package com.plasticene.base;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.plasticene.base.dao")
+@EnableRabbit    //要想监听队列接收消息必须开启此注解
 public class SmsFileServiceApplication {
 
     public static void main(String[] args) {
