8015b2e5c8bb3f3a6ebd68336a870b0155ccfcee
fjzheng <1059959730@qq.com>
2022-09-12 23:31:21 +0800

    update: :art: 优化代码注释

--- sms-file-service/src/main/java/com/plasticene/base/service/impl/SmsPlanServiceImpl.java
+++ sms-file-service/src/main/java/com/plasticene/base/service/impl/SmsPlanServiceImpl.java
@@ -9,8 +9,6 @@ import com.plasticene.base.service.SmsPlanService;
 import com.plasticene.base.service.SmsSendService;
 import com.plasticene.boot.common.utils.IdGenerator;
 import com.plasticene.boot.common.utils.PtcBeanUtils;
-import org.apache.commons.lang3.time.DateUtils;
-import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -49,20 +47,20 @@ public class SmsPlanServiceImpl implements SmsPlanService {
     /**
      * 创建短信计划之后，接下来就是异步调用短信平台发送短信及后续功能了，由于我们这里可以定时发送短信，即延时执行任务
      * 所以对分布式延时任务方案的确定之前，先看看延时任务和定时任务的区别
-     * ① 定时任务有明确的触发时间，延时任务没有
-     * ② 定时任务有执行周期，而延时任务在某事件触发后一段时间内执行，没有执行周期
-     * ③ 定时任务一般执行的是批处理操作是多个任务，而延时任务一般是单个任务
+     * 1. 定时任务有明确的触发时间，延时任务没有
+     * 2. 定时任务有执行周期，而延时任务在某事件触发后一段时间内执行，没有执行周期
+     * 3. 定时任务一般执行的是批处理操作是多个任务，而延时任务一般是单个任务
      *
      * 执行延时任务方案大概有三种：
-     * ① 定时扫描：启定时任务每隔一个时间段扫描一次，查询已经过了执行时间的任务进行执行，这里的扫描可以扫数据库或者redis，
+     * 1. 定时扫描：启定时任务每隔一个时间段扫描一次，查询已经过了执行时间的任务进行执行，这里的扫描可以扫数据库或者redis，
      *            取决于你怎么存储，如果用redis存储的话使用zset数据结构比较合适，执行时间为score
      *            缺点：执行时间不准确，有可能你刚刚你的任务快要到执行时间时，定时任务当前周期刚刚执行完，你需要等下一个周期
      *                 数据堆积，造成系统压力过大，有可能你在一个周期内要执行很多任务
-     * ② 监听redis key过期：我们可以通过计算任务定时执行时间，就是我们存入redis key的过期时间，这样我们就可以通过redis的消息
+     * 2. 监听redis key过期：我们可以通过计算任务定时执行时间，就是我们存入redis key的过期时间，这样我们就可以通过redis的消息
      *                     发布订阅功能去执行任务了
      *                     缺点：这种方式并不稳妥，redis的消息发布订阅不可靠，消息过期也不一定会马上删除监听到，这样也就造成执行时间
      *                          不准确，具体详情看{@link com.plasticene.base.listener.RedisKeyExpiredListener}
-     * ③ 消息队列之死信队列：当消息在一个队列中变成死信(dead message)之后，它能被重新发送到另一个交换机中，这个交换机就是DLX，绑定DLX的
+     * 3. 消息队列之死信队列：当消息在一个队列中变成死信(dead message)之后，它能被重新发送到另一个交换机中，这个交换机就是DLX，绑定DLX的
      *                    队列就称之为死信队列,rabbitmq能实现死信队列，详见{@link com.plasticene.base.config.RabbitmqConfig}
      *
      */
