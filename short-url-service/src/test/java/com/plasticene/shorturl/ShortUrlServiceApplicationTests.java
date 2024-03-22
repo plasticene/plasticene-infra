65711c263d023370e90f1327b671d2064caa33cf
fjzheng <1059959730@qq.com>
2024-03-22 10:59:04 +0800

    update：优化

--- short-url-service/src/test/java/com/plasticene/shorturl/ShortUrlServiceApplicationTests.java
+++ short-url-service/src/test/java/com/plasticene/shorturl/ShortUrlServiceApplicationTests.java
@@ -1,7 +1,9 @@
 package com.plasticene.shorturl;
 
+import com.plasticene.shorturl.entity.UrlLink;
 import com.plasticene.shorturl.service.ShortUrlService;
 import com.plasticene.shorturl.service.UniqueCodeService;
+import com.plasticene.shorturl.service.impl.ShortUrlServiceImpl;
 import com.plasticene.shorturl.utils.IpUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,15 +11,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class ShortUrlServiceApplicationTests {
 
     @Resource
-    private ShortUrlService shortUrlService;
+    private ShortUrlServiceImpl shortUrlService;
     @Resource
     private UniqueCodeService uniqueCodeService;
+
 
 
     void contextLoads() {
@@ -39,6 +44,16 @@ public class ShortUrlServiceApplicationTests {
     @Test
     public void test() {
         IpUtils.getIpRegion("115.206.246.88");
+    }
+
+    @Test
+    public void testBatchUpdate() {
+        List<UrlLink> list = shortUrlService.list();
+        list.forEach(urlLink -> {
+            urlLink.setUpdateTime(new Date());
+            urlLink.setUpdater(2l);
+        });
+        shortUrlService.updateBatchById(list);
     }
 
 }
